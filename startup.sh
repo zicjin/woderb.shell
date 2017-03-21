74beecdf9271972790fdf2ca34f4692771572130
zicjin <zicjin@gmail.com>
2017-03-21 16:21:31 +0800

    .

--- startup.sh
+++ startup.sh
@@ -1,8 +1,17 @@
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update && sudo apt-get install docker-ce
+
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs build-essential
 
+# https://yarnpkg.com/en/docs/install#linux-tab
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
-
-cd /root
