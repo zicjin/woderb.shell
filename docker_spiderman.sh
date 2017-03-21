91b0c32d863c9b5c252951a5f2249c2cbe8c32da
zicjin <zicjin@gmail.com>
2017-03-21 19:12:47 +0800

    .

--- docker_spiderman.sh
+++ docker_spiderman.sh
@@ -1,1 +1,1 @@
-docker run --rm --name spiderman.x -p 8080:80 -d registry.cn-hangzhou.aliyuncs.com/zicjin/spiderman.x $1
+docker run --name spiderman_$1 -d registry.cn-hangzhou.aliyuncs.com/zicjin/spiderman.x $1
