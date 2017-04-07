92153236cd3cbbd5fc0f20a77ff1c5c912b4ede3
zicjin <zicjin@gmail.com>
2017-04-07 15:32:41 +0800

    .

--- docker_mongo.sh
+++ docker_mongo.sh
@@ -1,2 +1,2 @@
 docker pull mongo
-docker run --rm -d --name mongo1 -p 27017:27017 -v /volumes/mdata:/data/db mongo --storageEngine wiredTiger
+docker run --rm -d --name mongo1 -p 27017:27017 -v /volumes/mdata:/data/db mongo
