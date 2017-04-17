c045f3c8dc002a32359c5313dbd29383bd992259
lokialone <648875862@qq.com>
2017-04-17 23:06:37 +0800

    .

--- docker_mongo.sh
+++ docker_mongo.sh
@@ -1,3 +1,3 @@
 docker pull mongo
 
-docker run --rm -d --name mongo1 -p 27017:27017 -v /volumes/mdata:/data/db mongo
+docker run --rm -d --name mongo1 -p 27017:27017 -v /volumes/mdata:/data/db mongo --auth
