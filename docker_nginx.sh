db9d2b7e37fb00489a54822b18935ef2c32e1e97
zicjin <zicjin@gmail.com>
2017-04-27 16:48:51 +0800

    .

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,6 +1,5 @@
 docker run --name nginx1 -p 80:80 \
   -v /root/woderb.web/dist:/usr/share/nginx/www:ro \
-  -v /root/woderb.web/dist_amp:/usr/share/nginx/www_amp:ro \
   -v /root/woderb.weather:/usr/share/nginx/weather:ro \
   -v /root/woderb.shell/nginx.conf:/etc/nginx/nginx.conf:ro \
   -d nginx
