5490e8c9230f6ec13da58195b0780000fcc7253b
zicjin <zicjin@gmail.com>
2017-04-27 19:52:35 +0800

    .

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,4 +1,4 @@
-docker run --name nginx1 -p 80:80 \
+docker run --name nginx1 -p 80:80 -p 443:443 \
   -v /root/ssl:/usr/share/nginx/ssl:ro \
   -v /root/woderb.web/dist:/usr/share/nginx/www:ro \
   -v /root/woderb.weather:/usr/share/nginx/weather:ro \
