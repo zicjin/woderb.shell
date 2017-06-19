6af2d6d08657d399e87623644c73c24d905b00d1
zicjin <zicjin@gmail.com>
2017-06-19 19:35:51 +0800

    .

--- docker_consul.sh
+++ docker_consul.sh
@@ -1,5 +1,5 @@
 docker pull consul
-docker run --rm --name consul1 \
+docker run -d --rm --name consul1 \
 	-p 8400:8400 -p 8500:8500/tcp -p 53:8600/udp \
 	-v /volumes/cdata:/consul/data \
 	-v /volumes/cconfig:/consul/config \
