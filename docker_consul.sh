99430155d885ac06e53c21a6d22b8314e217dc83
zicjin <zicjin@gmail.com>
2017-06-19 16:10:36 +0800

    .

--- docker_consul.sh
+++ docker_consul.sh
@@ -3,4 +3,5 @@ docker run --rm -d --name consul1 \
 	-p 8400:8400 -p 8500:8500/tcp -p 53:8600/udp \
 	-v /volumes/cdata:/consul/data \
 	-v /volumes/cconfig:/consul/config \
+	-e 'CONSUL_LOCAL_CONFIG={"acl_datacenter": "DC1", "acl_master_token": "z5656z", "acl_default_policy": "deny"}' \
     consul agent -server -node=n1 -bootstrap -ui -client="0.0.0.0" -bind='{{ GetPrivateIP }}'
