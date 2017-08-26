6c1bcd905dc13f77851eb80bdce92ba48abb2164
lokialone <648875862@qq.com>
2017-08-26 12:26:15 +0800

    .

--- startup.sh
+++ startup.sh
@@ -18,5 +18,6 @@ sudo apt-get update && sudo apt-get install yarn
 
 yarn global add cross-env pm2
 
+# https://askubuntu.com/a/510063
 wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
 sudo dpkg -i google-chrome-beta_current_amd64.deb
