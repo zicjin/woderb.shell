92eb3aebd2c147d463db4b4df94faaf8eade482e
Cheney Jin <jzc@vultr.guest>
2017-10-18 06:34:20 +0000

    .

--- docker_redis.sh
+++ docker_redis.sh
@@ -1,6 +1,6 @@
 docker pull redis
 
-docker run --rm --name redis1 -p 16379:6379 \
+docker run -d --rm --name redis1 -p 16379:6379 \
     -v /volumes/rdata:/data \
     -v redis.conf:/etc/redis/redis.conf \
     redis redis-server /etc/redis/redis.conf --appendonly yes
