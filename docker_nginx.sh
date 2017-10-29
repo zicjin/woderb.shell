e11609d6de7bb5ce4ed11e9bf60b9b28433096dc
lokialone <648875862@qq.com>
2017-10-29 15:41:17 +0800

    .

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -4,3 +4,4 @@ docker run -d --rm --name nginx1 -p 80:80 -p 443:443 \
   -v /root/woderb.weather:/usr/share/nginx/weather:ro \
   -v /root/woderb.shell/nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx
+
