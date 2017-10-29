a52062109b196c16e67df29214d451153a8095b1
lokialone <648875862@qq.com>
2017-10-29 13:28:08 +0800

    .

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,6 +1,6 @@
-docker run --name nginx1 -p 80:80 -p 443:443 \
+docker run -d --rm --name nginx1 -p 80:80 -p 443:443 \
   -v /root/ssl:/usr/share/nginx/ssl:ro \
   -v /root/woderb.web/dist:/usr/share/nginx/www:ro \
   -v /root/woderb.weather:/usr/share/nginx/weather:ro \
   -v /root/woderb.shell/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -d nginx
+  nginx
